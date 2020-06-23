53f4afd92337d6b1edffabe1a0e853bd59b26d19
Khusharth <khusharth19@gmail.com>
2020-06-24 04:38:51 +0530

    Added Game Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import "../../styles/global.scss";
 import history from "../../history";
 import { Home, Instructions, ChooseTheme, GamePage } from "../../pages";
 import { Header } from "../../components";
 import { ThemeContext } from "../../ThemeContext";
+import useTheme from "../../useTheme";
 
 const App = () => {
-    const [theme, setTheme] = useState("blue");
+    const [theme, setTheme] = useTheme("blue", "Theme");
+
+    useEffect(() => {
+        const getTheme = localStorage.getItem('Theme');
+        console.log(getTheme);
+        if (getTheme === 'green') {
+            setTheme('green');
+        } else if (getTheme === 'brown') {
+            setTheme('brown');
+        } else {
+            setTheme('blue');
+        }
+    }, []);
 
     return (
         <Router history={history}>
             <div className={`theme-${theme}`}>
-                <Header />
-                <Switch>
-                    <ThemeContext.Provider value={{ theme, setTheme }}>
+                <ThemeContext.Provider value={{ theme, setTheme }}>
+                    <Header />
+                    <Switch>
+
                         <Route path='/' exact component={Home} />
                         <Route path='/instructions' component={Instructions} />
                         <Route path='/theme' component={ChooseTheme} />
                         <Route path='/game' component={GamePage} />
-                    </ThemeContext.Provider>
-                </Switch>
+                    </Switch>
+                </ThemeContext.Provider>
             </div>
         </Router>
     );
--- src/pages/GamePage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GamePage = () => {
-    return <div>Game page</div>;
-};
-
-export default GamePage;

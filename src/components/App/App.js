cb91dbc5b3ed3e70bc97e4b0228380c703e630d3
Khusharth <khusharth19@gmail.com>
2020-06-23 23:54:46 +0530

    COnfigured Router for the project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import "../../styles/global.scss";
 import history from "../../history";
 import { Home, Instructions, ChooseTheme, GamePage } from "../../pages";
 import { Header } from "../../components";
+import { ThemeContext } from "../../ThemeContext";
 
 const App = () => {
+    const [theme, setTheme] = useState("blue");
+
     return (
         <Router history={history}>
-            <Header />
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/instructions' component={Instructions} />
-                <Route path='/theme' component={ChooseTheme} />
-                <Route path='/game' component={GamePage} />
-            </Switch>
+            <div className={`theme-${theme}`}>
+                <Header />
+                <Switch>
+                    <ThemeContext.Provider value={{ theme, setTheme }}>
+                        <Route path='/' exact component={Home} />
+                        <Route path='/instructions' component={Instructions} />
+                        <Route path='/theme' component={ChooseTheme} />
+                        <Route path='/game' component={GamePage} />
+                    </ThemeContext.Provider>
+                </Switch>
+            </div>
         </Router>
     );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Header from "./Header/Header";
 import Logo from "./Logo/Logo";
 import Modal from "./Modal/Modal";
 import Button from "./Button/Button";
+import ThemeCard from "./ThemeCard/ThemeCard";
+import Card from "./Card/Card";
+import Board from "./Board/Board";
+import Rodal from "./Rodal/Rodal";
 
-export { App, Header, Logo, Modal, Button };
+export { App, Header, Logo, Modal, Button, ThemeCard, Card, Board, Rodal };
